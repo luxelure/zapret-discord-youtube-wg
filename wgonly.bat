971876ddb132626163f93fd84b2c39b649ffd4e1
luxelure <79971443+luxelure@users.noreply.github.com>
2025-04-22 15:44:09 +0700

    siberian moment

--- wgonly.bat
+++ wgonly.bat
@@ -7,7 +7,6 @@ call service_status.bat zapret
 echo:
 
 set "BIN=%~dp0bin\"
-set "LISTS=%~dp0lists\"
 
 start "zapret: wgonly" /min "%BIN%winws.exe" --wf-udp=53-65535 ^
 --filter-udp=53-65535 --filter-l7=wireguard --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-unknown-udp="%BIN%wireguard_initiation.bin"
